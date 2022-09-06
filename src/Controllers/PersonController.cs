9a34634aaf692b7f509c36fe90d34d701e426893
Glyson <kauaglyson@gmail.com>
2022-09-06 00:09:29 -0300

    Person API Controller

---
+++ src/Controllers/PersonController.cs
@@ -0,0 +1,14 @@
+// Importing ASP.Net Core
+using Microsoft.AspNetCore.Mvc;
+
+namespace src.Controllers;
+
+//  Saying the class bellow (PersonController) is an API
+[ApiController]
+/* Creating a new Route. The "[controller]" will be replaced by
+the controller class name (PersonController)  */
+[Route("[controller")]
+public class PersonController
+{
+
+}
