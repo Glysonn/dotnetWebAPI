c5e1b42004d0cdffcac067e54b884c45dd52e369
Glyson <kauaglyson@gmail.com>
2022-09-08 19:52:43 -0300

    Contract model

---
+++ src/Models/Contract.cs
@@ -0,0 +1,24 @@
+namespace src.Models;
+
+public class Contract
+{
+    //  Constructor
+    public Contract()
+    {
+        this.CreateData = DateTime.Now;
+        this.Value = 0;
+        this.TokenId = "00000";
+        this.WasPayed = false;
+    }
+    public Contract(double Value, string TokenId)
+    {
+        this.CreateData = DateTime.Now;
+        this.Value = Value;
+        this.TokenId = TokenId;
+    }
+    public DateTime CreateData { get; set; }
+    public string TokenId { get; set; }
+    public double Value { get; set; }
+    public bool WasPayed { get; set; }
+
+}
