7fd853f261d9fd461dabe54f0905d09bbe9d2d70
Glyson <kauaglyson@gmail.com>
2022-09-11 19:00:56 -0300

    Setting tables/entities to DB

--- src/Persistence/DatabaseContext.cs
+++ src/Persistence/DatabaseContext.cs
@@ -5,13 +5,26 @@ namespace src.Persistence;
 
 public class DatabaseContext : DbContext
 {
-    public DatabaseContext (DbContextOptions<DatabaseContext> options)
+
+        // "base" means the base class (the class it was inherited from, in this case DbContext).
+        // I'm passing the var options as parameter
+    public DatabaseContext (DbContextOptions<DatabaseContext> options) : base(options)
     {
 
     }
+
     //  declaring what are the entities in a Data Base context
     public DbSet<Contract> Contratos { get; set; }
     public DbSet<Person> Pessoas { get; set; }
 
 
+    // OnModelCreating is a method that defines configurations when it's turning to a database context 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        //  Contract model now is a Table
+        builder.Entity<Contract> (e =>{});
+
+        //  Person model now is a Table
+        builder.Entity<Person> (e=>{});
+    }
 }
