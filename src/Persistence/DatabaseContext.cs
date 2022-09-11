9f58c0cefcaa71a3ab27d12224c31a30b982a32b
Glyson <kauaglyson@gmail.com>
2022-09-11 19:00:14 -0300

    Setting tables/entities to DB

--- src/Persistence/DatabaseContext.cs
+++ src/Persistence/DatabaseContext.cs
@@ -1,8 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using src.Models;
 
 namespace src.Persistence;
 
-public class DbContext
+public class DatabaseContext : DbContext
 {
+    public DatabaseContext (DbContextOptions<DatabaseContext> options)
+    {
+
+    }
+    //  declaring what are the entities in a Data Base context
+    public DbSet<Contract> Contratos { get; set; }
+    public DbSet<Person> Pessoas { get; set; }
+
 
 }
