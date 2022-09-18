38c33fa8bc26f407c181d8455596aceb6bccf8ad
Glyson <kauaglyson@gmail.com>
2022-09-18 16:16:20 -0300

    refactoring and treating exceptions
    Refactored all http methods and threw some exceptions

--- src/Controllers/PersonController.cs
+++ src/Controllers/PersonController.cs
@@ -1,16 +1,15 @@
-//  Importing ASP.Net Core
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-//  Importing Models namespace
+
 using src.Models;
 using src.Persistence;
 
+
 namespace src.Controllers;
 
-//  Saying the class bellow (PersonController) is an API
 [ApiController]
-/* Creating a new Route. The "[controller]" will be replaced by
-the controller class name (PersonController)  */
+
 [Route("[controller]")]
 public class PersonController : ControllerBase
 {
@@ -22,39 +21,70 @@ public class PersonController : ControllerBase
         this._context = context;
     }
 
-    //  Using the Get HTTP method
     [HttpGet]
-    //  Creating a Person (model class) method type
-    public List<Person> GetPessoa()
+    public ActionResult<List<Person>> GetPessoa()
     {
-        _context.Pessoas.ToList();
-        //  Returning the object is only possible 'cause the method type is Person (model class)
-        return _context.Pessoas.Include( p => p.Contracts).ToList();
+        var result = _context.Pessoas.Include( p => p.Contracts).ToList();
+        return (result.Any()) ? Ok(result) : NoContent();
     }
 
     [HttpPost]
-    public Person PostPessoa(Person pessoa)
+    public ActionResult<Person> PostPessoa(Person pessoa)
     {
+        try
+        {
         _context.Pessoas.Add(pessoa);
         _context.SaveChanges();
-        return pessoa;
+        }
+        catch
+        {
+            return BadRequest(new{
+                Status = "ERROR: You did a bad request!",
+                Status_Code = HttpStatusCode.BadRequest });
+        }
+        return Created("Your request was successful", pessoa);
     }
 
     [HttpPut("{id}")]
-    public string UpdatePessoa([FromRoute]int id, [FromBody]Person pessoa)
-    {
-        _context.Update(pessoa);
-        _context.SaveChanges();
-        return $"ID {id} from Person {pessoa.Name} status: updated.";
-    }
-
-    [HttpDelete ("{id}")]
-    public string DeletePessoa([FromRoute]int id)
+    public ActionResult<Object> UpdatePessoa([FromRoute]int id, [FromBody]Person pessoa)
     {
         var result = _context.Pessoas.SingleOrDefault( e => e.Id == id);
-        _context.Remove(result);
+        if (result is null) return NotFound(new{
+            Status="ERROR: Your ID doesn't existis!",
+            Status_Code = HttpStatusCode.NotFound});
+        try
+        {
+        _context.Update(pessoa);
         _context.SaveChanges();
-        return $"The person which ID was {id} was deleted.";
+        }
+        catch
+        {
+            return BadRequest(new{
+                Status = "Error: You did a bad request!",
+                Status_Code = HttpStatusCode.BadRequest});
+        }
+
+        return Accepted(new{
+            Status = $"ID {id} from Person {pessoa.Name} was: updated.",
+            Status_Code = HttpStatusCode.Accepted});
     }
 
+    
+
+    [HttpDelete ("{id}")]
+    public ActionResult<Object> DeletePessoa([FromRoute]int id)
+    {
+        var result = _context.Pessoas.SingleOrDefault( e => e.Id == id);
+        if (result is null) return BadRequest(new {
+            Status = "Error: You did a bad request!",
+            Status_Code = HttpStatusCode.BadRequest});
+
+        _context.Remove(result);
+        _context.SaveChanges();
+
+        return Ok(new{Status= $"The ID {id} person was deleted.",
+        Status_Code = HttpStatusCode.OK});
+
+    }
+    
 }
